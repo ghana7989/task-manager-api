c877e3d9f803b707884c2a913c907240033bc5db
ghana <ghana.chindukuri@gmail.com>
2021-04-16 20:44:33 +0530

    Adding users and tasks

---
+++ src/db/mongoose.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
+})
