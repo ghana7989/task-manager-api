647f40c48e9aa236584cbeed338c02807cdfd5f2
ghana <ghana.chindukuri@gmail.com>
2021-04-17 11:20:45 +0530

    adding file uploads feature

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,9 +3,13 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGO_URI, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true,
-})
+mongoose.connect(
+	process.env.MONGO_URI,
+	{
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	},
+	() => console.log('Mongodb connected successfully'),
+)
