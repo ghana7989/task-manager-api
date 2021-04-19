11b658252b15b543064983c3769ef4df4647cf1b
ghana <ghana.chindukuri@gmail.com>
2021-04-19 09:57:43 +0530

    🎨  adding more tests

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,19 +1,17 @@
-/** @format */
-
 const express = require('express')
 const multer = require('multer')
 const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
-// const {sendWelcomeEmail, sendCancellationEmail} = require('../emails/account')
+const {sendWelcomeEmail, sendCancellationEmail} = require('../emails/account')
 
 router.post('/users', async (req, res) => {
 	const user = new User(req.body)
 
 	try {
 		await user.save()
-		// sendWelcomeEmail(user.email, user.name)
+		sendWelcomeEmail(user.email, user.name)
 		const token = await user.generateAuthToken()
 		res.status(201).send({user, token})
 	} catch (e) {
@@ -81,7 +79,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
 	try {
 		await req.user.remove()
-		// sendCancellationEmail(req.user.email, req.user.name)
+		sendCancellationEmail(req.user.email, req.user.name)
 		res.send(req.user)
 	} catch (e) {
 		res.status(500).send()
